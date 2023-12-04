c07e64862187fee0d7164fc5403378765b6fd047
jtaren <arenjatau@gmail.com>
2023-12-03 20:40:21 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if "c" not in my_string and "C" not in my_string:
+        return my_string
+    else:
+        new_string = ""
+        for char in my_string:
+            if char != "c" and char != "C":
+                new_string += char
+                return new_string
