3df9b283963298a6283912736c451e4959352bbd
jtaren <arenjatau@gmail.com>
2023-12-18 04:01:01 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,12 +10,12 @@ def safe_print_list(my_list=[], x=0):
   Returns:
     The number of elements actually printed.
   """
-    printed_count = 0
+  ret = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            printed_count += 1
+            ret += 1
         except IndexError:
             break
     print("")
-    return (printed_count)
+    return (ret)
