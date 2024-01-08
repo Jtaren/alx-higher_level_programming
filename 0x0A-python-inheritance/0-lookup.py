7035824f5c276b4ef434365b9fb9202f7b5eea43
jtaren <arenjatau@gmail.com>
2024-01-08 03:55:31 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,27 +1,6 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
 def lookup(obj):
-  """
-  Returns a list of available attributes and methods of an object.
-
-  Args:
-      obj: The object to be inspected.
-
-  Returns:
-      A list containing both attributes and methods of the object.
-  """
-  attributes = dir(obj)
-  methods = [method for method in attributes if callable(getattr(obj, method))]
-  return attributes + methods
-
-# Example usage
-class MyClass:
-  def __init__(self, name):
-    self.name = name
-
-  def say_hello(self):
-    print(f"Hello from {self.name}!")
-
-obj = MyClass("John")
-
-available_elements = lookup(obj)
-
-print(f"Available elements for object {obj}: {available_elements}")
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
