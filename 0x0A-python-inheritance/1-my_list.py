fca08ece31e2968cef2c737931c96f9f5cf0609f
jtaren <arenjatau@gmail.com>
2024-01-08 04:19:02 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,15 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
 
 class MyList(list):
-  """
-  MyList class inherits from list and adds a print_sorted method.
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
-  Attributes:
-      (inherited from list)
-
-  Methods:
-      print_sorted(): Prints the list elements sorted in ascending order.
-  """
-
-  def print_sorted(self):
-    """
-    Prints the list elements sorted in ascending order.
-    """
-    print(f"Sorted list: {sorted(self)}")
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
