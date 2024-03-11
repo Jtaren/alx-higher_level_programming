c15ba97927b47e8a965d1201ebe289443413c7ed
jtaren <arenjatau@gmail.com>
2024-03-11 07:43:37 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let i;
+let count = 0;
+for (i = 0; i < process.argv.length; i++) {
+  count++;
+}
+if (count < 3) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
