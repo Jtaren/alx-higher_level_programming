ce3e5883dcbf507ef330dd8f99908862a03cfa86
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-29 05:26:35 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a POST request to the URL with specified parameters and display the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# Script that sends a POST request and displays the body response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
