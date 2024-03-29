1769825e57ff33fd4fa59c4b3a4c2ac10e86d184
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-29 05:17:32 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Check if the number of arguments is not equal to 2
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
