dffd3cad58f231ad2092985724efeed7d584de4c
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-29 05:32:57 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-import urllib.request
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib
+"""
+
+
+from urllib import request
 
 url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html)
-    print("\t- utf8 content:", html.decode('utf-8'))
+with request.urlopen(url) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+
+print("Body response:")
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
+print("\t- utf8 content: {}".format(utf8_content))
