14acc12a2caf34cef97b8f4220536678c508289d
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-29 05:43:51 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""A script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
 """
-import sys
+Uses Basic Authentication with a personal access token
+to access the GitHub API and display the user ID.
+"""
+
 import requests
-from requests.auth import HTTPBasicAuth
+import sys
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    auth = (username, password)
+    response = requests.get(url, auth=auth)
+
+    try:
+        json_data = response.json()
+        print(json_data.get('id'))
+    except ValueError:
+        print(None)
