5dc10da39edf034155db8acbbff27674d9a88af1
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2024-03-29 05:21:05 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that take in a URL, sends a request to it, and displays the size of the body of the response, size should be displayed in bytes
-curl -s "$1" | wc -c
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
